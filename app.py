1a1bc7d36f2cc2c1ece6f501066592f7486a8c74
aledmorris <aled.morris@pexip.com>
2021-12-16 23:10:18 +1100

    first commit

---
+++ app.py
@@ -0,0 +1,13 @@
+#
+
+import requests
+import getpass
+
+print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+print('     OBTP Tester')
+print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+print('\n')
+username = input('Enter username: ')
+password = getpass.getpass()
+
+print(f'Username = {username}, Password = {password}')
