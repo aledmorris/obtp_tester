b50e8a86f5aa7dfa4c79f14da1876706140885af
aledmorris <aled.morris@pexip.com>
2021-12-18 18:33:05 +1100

    Working but time issues

--- cli.py
+++ cli.py
@@ -2,6 +2,8 @@ import requests
 from xml.dom.minidom import parse, parseString
 import urllib3
 from getpass import getpass
+import datetime
+from datetime import timedelta
 
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
@@ -43,8 +45,13 @@ def display_menu():
 def make_booking(booking_title,start_time,duration):
     
     #calculat the start time, end time, using the duration and timezone
-    utc_start = ''
-    utc_end = ''
+    current_time = datetime.datetime.now()
+
+    utc_start = current_time.strftime("%Y-%m-%dT") + start_time + ":00Z"
+    utc_start_dtobj = datetime.datetime.strptime(utc_start, '%Y-%m-%dT%H:%M:%SZ')
+
+    utc_end_dtobj = utc_start_dtobj + timedelta(minutes=int(duration))
+    utc_end = utc_end_dtobj.strftime("%Y-%m-%dT%H:%M:%SZ")
     
     # xml payload = booking properties
     example_xml = "<?xml version='1.0'?>\r\n\t<Bookings item=\"1\" status=\"OK\">\r\n\t\t<Booking item=\"1\">\r\n\t\t\t<Id item=\"1\">1</Id>\r\n\t\t\t<Title item=\"1\">"+booking_title+"</Title>\r\n\t\t\t<Agenda item=\"1\"></Agenda>\r\n\t\t\t<Privacy item=\"1\">Public</Privacy>\r\n\t\t\t<Organizer item=\"1\">\r\n\t\t\t\t<FirstName item=\"1\">"+firstname+"</FirstName>\r\n\t\t\t\t<LastName item=\"1\">"+lastname+"</LastName>\r\n\t\t\t\t<Email item=\"1\">"+email+"</Email>\r\n\t\t\t</Organizer>\r\n\t\t\t<Time item=\"1\">\r\n\t\t\t\t<StartTime item=\"1\">"+utc_start+"</StartTime>\r\n\t\t\t\t<StartTimeBuffer item=\"1\">"+start_buffer+"</StartTimeBuffer>\r\n\t\t\t\t<EndTime item=\"1\">"+utc_end+"</EndTime>\r\n\t\t\t\t<EndTimeBuffer item=\"1\">"+end_buffer+"</EndTimeBuffer>\r\n\t\t\t</Time>\r\n\t\t\t<MaximumMeetingExtension item=\"1\">10</MaximumMeetingExtension>\r\n\t\t\t<BookingStatus item=\"1\">OK</BookingStatus>\r\n\t\t\t<BookingStatusMessage item=\"1\"></BookingStatusMessage>\r\n\t\t\t<Webex item=\"1\">\r\n\t\t\t\t<Enabled item=\"1\">False</Enabled>\r\n\t\t\t\t<MeetingNumber item=\"1\"></MeetingNumber>\r\n\t\t\t\t<Password item=\"1\"></Password>\r\n\t\t\t</Webex>\r\n\t\t\t<Encryption item=\"1\">BestEffort</Encryption>\r\n\t\t\t<Role item=\"1\">Master</Role>\r\n\t\t\t<Recording item=\"1\">Disabled</Recording>\r\n\t\t\t<DialInfo item=\"1\">\r\n\t\t\t\t<Calls item=\"1\">\r\n\t\t\t\t\t<Call item=\"1\">\r\n\t\t\t\t\t\t<Number item=\"1\">"+uri+"</Number>\r\n\t\t\t\t\t\t<Protocol item=\"1\">"+proto+"</Protocol>\r\n\t\t\t\t\t\t<CallRate item=\"1\">"+call_rate+"</CallRate>\r\n\t\t\t\t\t\t<CallType item=\"1\">"+call_type+"</CallType>\r\n\t\t\t\t\t</Call>\r\n\t\t\t\t</Calls>\r\n\t\t\t\t<ConnectMode item=\"1\">OBTP</ConnectMode>\r\n\t\t\t</DialInfo>\r\n\t\t</Booking>\r\n\t</Bookings>\r\n"
@@ -115,14 +122,16 @@ if auth_response is '204' or '200':
         
         choice = input('> Your selection: ')
         
-        if choice == 1:
+        if choice == str(1):
             # do stuff to add a booking to the endpoint
             print('\nLet\'s add a booking...')
                                    
             # Booking Title
             booking_title = input('> Booking title: ')
             # start time
-            start_time = input('> Start time HH:MM 24 hrs (assumes today\'s date): ')
+            current_time = datetime.datetime.now()
+            print(f'[i] Current UTC time is {current_time}')
+            start_time = input('> Start time HH:MM (UTC): ')
             # end time (calculated)
             duration = input('> Duration (mins): ')                 
             
@@ -130,7 +139,7 @@ if auth_response is '204' or '200':
             
             
             
-        elif choice == 2:
+        elif choice == str(2):
             # exit the loop
             looping=False
             print('\n>>...Exiting the app....')
